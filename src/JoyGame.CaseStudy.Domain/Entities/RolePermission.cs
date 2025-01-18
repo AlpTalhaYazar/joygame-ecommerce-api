ce714d36041dc10d192c4e969dbb37273ed0256a
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-18 21:06:23 +0300

    feat: add RolePermission and UserRole entities for role-based access control

---
+++ src/JoyGame.CaseStudy.Domain/Entities/RolePermission.cs
@@ -0,0 +1,10 @@
+namespace JoyGame.CaseStudy.Domain.Entities;
+
+public class RolePermission
+{
+    public int RoleId { get; set; }
+    public int PermissionId { get; set; }
+
+    public virtual Role Role { get; set; }
+    public virtual Permission Permission { get; set; }
+}
