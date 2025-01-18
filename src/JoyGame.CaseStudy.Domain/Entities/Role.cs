85c22b177b1a2580f61b1088c38e1b4c171047f5
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-19 00:02:19 +0300

    feat: make Name property required and initialize collections in Permission and Role entities

--- src/JoyGame.CaseStudy.Domain/Entities/Role.cs
+++ src/JoyGame.CaseStudy.Domain/Entities/Role.cs
@@ -4,16 +4,10 @@ namespace JoyGame.CaseStudy.Domain.Entities;
 
 public class Role : BaseEntity
 {
-    public string Name { get; set; }
-    public string? Description { get; set; }
+    public required string Name { get; set; }
+    public string Description { get; set; } = string.Empty;
 
     // Navigation properties
-    public virtual ICollection<UserRole> UserRoles { get; set; }
-    public virtual ICollection<RolePermission> RolePermissions { get; set; }
-
-    public Role()
-    {
-        UserRoles = new HashSet<UserRole>();
-        RolePermissions = new HashSet<RolePermission>();
-    }
+    public virtual ICollection<UserRole> UserRoles { get; set; } = new HashSet<UserRole>();
+    public virtual ICollection<RolePermission> RolePermissions { get; set; } = new HashSet<RolePermission>();
 }
