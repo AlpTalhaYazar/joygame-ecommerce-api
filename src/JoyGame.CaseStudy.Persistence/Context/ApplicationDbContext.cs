038c68803074756eb18d3392f527797118299761
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-19 10:19:31 +0300

    feat: enhance ApplicationDbContext with DbSet properties and query filters

--- src/JoyGame.CaseStudy.Persistence/Context/ApplicationDbContext.cs
+++ src/JoyGame.CaseStudy.Persistence/Context/ApplicationDbContext.cs
@@ -1,6 +1,48 @@
+using JoyGame.CaseStudy.Domain.Common;
+using JoyGame.CaseStudy.Domain.Entities;
+using JoyGame.CaseStudy.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
 namespace JoyGame.CaseStudy.Persistence.Context;
 
-public class ApplicationDbContext
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<RolePermission> RolePermissions { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        modelBuilder.Entity<User>().HasQueryFilter(x => x.Status != EntityStatus.Deleted);
+        modelBuilder.Entity<Category>().HasQueryFilter(x => x.Status != EntityStatus.Deleted);
+        modelBuilder.Entity<Product>().HasQueryFilter(x => x.Status != EntityStatus.Deleted);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                    entry.Entity.CreatedBy ??= "System";
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedAt = DateTime.UtcNow;
+                    entry.Entity.LastModifiedBy ??= "System";
+                    break;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
