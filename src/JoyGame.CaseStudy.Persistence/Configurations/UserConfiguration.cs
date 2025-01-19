4f61c1c66a93430b0ace6d7ab6c71c136602fab0
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-19 10:57:32 +0300

    feat: implement UserConfiguration with required properties and unique indexes

--- src/JoyGame.CaseStudy.Persistence/Configurations/UserConfiguration.cs
+++ src/JoyGame.CaseStudy.Persistence/Configurations/UserConfiguration.cs
@@ -1,6 +1,36 @@
+using JoyGame.CaseStudy.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace JoyGame.CaseStudy.Persistence.Configurations;
 
-public class UserConfiguration
+public class UserConfiguration : IEntityTypeConfiguration<User>
 {
-    
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Username)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.PasswordHash)
+            .IsRequired();
+
+        builder.Property(x => x.FirstName)
+            .HasMaxLength(50);
+
+        builder.Property(x => x.LastName)
+            .HasMaxLength(50);
+
+        // Add unique indexes
+        builder.HasIndex(x => x.Username).IsUnique();
+        builder.HasIndex(x => x.Email).IsUnique();
+    }
 }
