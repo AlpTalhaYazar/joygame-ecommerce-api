2063f1a7c19454a184280fe0f7ea316d4d282859
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-19 11:01:26 +0300

    feat: configure Category entity with required properties and unique index for slug

--- src/JoyGame.CaseStudy.Persistence/Configurations/CategoryConfiguration.cs
+++ src/JoyGame.CaseStudy.Persistence/Configurations/CategoryConfiguration.cs
@@ -1,6 +1,35 @@
+using JoyGame.CaseStudy.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace JoyGame.CaseStudy.Persistence.Configurations;
 
-public class CategoryConfiguration
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
 {
-    
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(500);
+
+        builder.Property(x => x.Slug)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        // Configure self-referencing relationship for hierarchical categories
+        builder.HasOne(x => x.Parent)
+            .WithMany(x => x.Children)
+            .HasForeignKey(x => x.ParentId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Add unique index for slug
+        builder.HasIndex(x => x.Slug).IsUnique();
+    }
 }
