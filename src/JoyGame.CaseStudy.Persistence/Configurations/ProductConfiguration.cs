7979018698687e8e0a85f66dac7389277141fd9d
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-19 11:01:35 +0300

    feat: configure Product entity with required properties and unique index for slug

--- src/JoyGame.CaseStudy.Persistence/Configurations/ProductConfiguration.cs
+++ src/JoyGame.CaseStudy.Persistence/Configurations/ProductConfiguration.cs
@@ -1,6 +1,42 @@
+using JoyGame.CaseStudy.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace JoyGame.CaseStudy.Persistence.Configurations;
 
-public class ProductConfiguration
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
 {
-    
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(2000);
+
+        builder.Property(x => x.Slug)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.Price)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Property(x => x.ImageUrl)
+            .HasMaxLength(500);
+
+        // Configure relationship with Category
+        builder.HasOne(x => x.Category)
+            .WithMany(x => x.Products)
+            .HasForeignKey(x => x.CategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Add unique index for slug
+        builder.HasIndex(x => x.Slug).IsUnique();
+    }
 }
