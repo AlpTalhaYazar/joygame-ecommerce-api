45adb264812d1943c66d8c3c10c1a58988ec0832
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-19 21:20:02 +0300

    feat: implement IDesignTimeDbContextFactory for ApplicationDbContext with configuration loading

--- src/JoyGame.CaseStudy.Persistence/Context/ApplicationDbContextFactory.cs
+++ src/JoyGame.CaseStudy.Persistence/Context/ApplicationDbContextFactory.cs
@@ -1,6 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
 namespace JoyGame.CaseStudy.Persistence.Context;
 
-public class ApplicationDbContextFactory
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
 {
-    
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ??
+                               "Server=localhost;Database=JoyGameCaseStudy;User Id=sa;Password=YourPassword;TrustServerCertificate=True";
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
 }
